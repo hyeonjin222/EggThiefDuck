245898b3adac495f4ad3e9c3ae8d2e8569831117
HJ <lhj38071@gmail.com>
2026-05-12 19:45:50 +0900

    feat. 첫 프로젝트 생성

---
+++ Source/EggThiefDuck/EggThiefDuck.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EggThiefDuck.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EggThiefDuck, "EggThiefDuck" );
