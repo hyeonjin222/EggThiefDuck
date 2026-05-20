5855fe6f1002c3ab844ec67e0cf07eaa99d77ad6
HJ <lhj38071@gmail.com>
2026-05-20 20:41:44 +0900

    asdfasdf

--- Source/EggThiefDuck/Private/UI/MainHUDWidget.cpp
+++ Source/EggThiefDuck/Private/UI/MainHUDWidget.cpp
@@ -1,3 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UI/MainHUDWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+// 로직 롤백: 특수 연출 코드 모두 제거
